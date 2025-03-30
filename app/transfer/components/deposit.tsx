7a37305d390c2d7a4e7d11a3191e455ff93cb1c4
Rohitparakh <rohitparakh4@gmail.com>
2025-03-30 19:23:29 +0530

    ui

--- app/transfer/components/deposit.tsx
+++ app/transfer/components/deposit.tsx
@@ -3,16 +3,19 @@
 import { mono } from "@/app/fonts";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { isValidSolanaAddress, setSolBalance } from "@/lib/utils";
 import { toast } from "react-toastify";
 import { useRouter } from "next/navigation";
+import { useWalletAssets } from "@/hooks/useWalletAssets";
+import { formatNumber } from "@/lib/utils";
 
 export default function Deposit() {
   const [walletAddr, setWalletAddr] = useState<string>(
     "8rrSVS8Uosk2FvNipEkiyJTULajabRQ9EDLbWKrh79mZ",
   );
   const [balanceInput, setBalanceInput ] = useState<number | string | undefined>("");
+    const { data, isLoading, refetch } = useWalletAssets();
 
   const router = useRouter();
 
@@ -20,7 +23,12 @@ export default function Deposit() {
     router.refresh(); // This refreshes the current route and revalidates server components
   };
 
+  const [balance, setBalance] = useState<number>(data?.solBalance || 0);
+  const [currency, setCurrency] = useState<string>("SOL");
 
+ useEffect(() => {
+    setBalance(data?.solBalance);
+  }, [data])
   const handleBalanceInput = async (e: React.KeyboardEvent<HTMLInputElement>) => {
       if (e.key === "Enter") {      
         const res = await setSolBalance(Number(balanceInput))
@@ -44,13 +52,15 @@ export default function Deposit() {
 
   return (
     <Card>
-      {/* <CardHeader>
+      <CardHeader>
         <CardTitle className="!font-normal">
           Deposit to your Titan trading wallet.
         </CardTitle>
-      </CardHeader> */}
-      <CardContent className="flex flex-wrap gap-4 items-end mt-4">
-        Write the required SOL balance:
+      </CardHeader>
+      <CardContent className="flex flex-wrap gap-4 items-center mt-4">
+        Current SOL balance: <span className="text-[#8c003e] font-[900] text-[18px]">{formatNumber(balance).value}
+                      {formatNumber(balance).unit} {currency}
+                      </span>
         <Input 
         className={mono.className}
         placeholder="Change Balance"
