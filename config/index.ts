62a1a6a7d4db002ec3dbfc8ded32e7b68bf738ff
Rohitparakh <rohitparakh4@gmail.com>
2025-04-07 00:13:30 +0530

    added env

--- config/index.ts
+++ config/index.ts
@@ -1,3 +1,4 @@
 export const API_URL = process.env.NEXT_PUBLIC_API_URL as string
 export const BOT_URL = process.env.NEXT_PUBLIC_BOT_URL as string
 export const SOCKET_SERVER = process.env.NEXT_PUBLIC_SOCKET_SERVER_URL as string
+export const DISCORD_AUTH_URL = process.env.NEXT_PUBLIC_DISCORD_AUTH_URL as string
